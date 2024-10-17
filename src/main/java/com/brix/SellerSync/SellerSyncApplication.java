5db28c86961825b5fdc394e77d2c13cdd0edd7bf
Brixter Porras <brixterporras@gmail.com>
2024-10-17 11:41:26 +0800

    feat: added initial database connection

--- src/main/java/com/brix/SellerSync/SellerSyncApplication.java
+++ src/main/java/com/brix/SellerSync/SellerSyncApplication.java
@@ -1,13 +1,33 @@
 package com.brix.SellerSync;
 
+import javax.sql.DataSource;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 
 @SpringBootApplication
-public class SellerSyncApplication {
+@Log
+public class SellerSyncApplication implements CommandLineRunner{
+	
+	private final DataSource dataSource;
+
+	public SellerSyncApplication(final DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SellerSyncApplication.class, args);
+	}
+	
+	@Override
+	public void run(final String... args){
+		// log.info("DataSource: " + dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+		restTemplate.execute("select 1");
 	}
 
 }
