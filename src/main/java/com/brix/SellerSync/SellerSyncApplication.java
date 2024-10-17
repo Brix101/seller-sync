3719283ba930910088253ffbe64897825db0721c
Brixter Porras <brixterporras@gmail.com>
2024-10-17 11:03:17 +0800

    first commit

---
+++ src/main/java/com/brix/SellerSync/SellerSyncApplication.java
@@ -0,0 +1,13 @@
+package com.brix.SellerSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brix/SellerSync/SellerSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brix.SellerSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
