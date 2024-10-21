5bf931e66fcdb18a84974ee946d6de56a26bc97f
Brixter Porras <brixterporras@gmail.com>
2024-10-21 11:30:55 +0800

    feat: added hello world

---
+++ src/main/java/com/brix/SellerSync/hello/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.brix.SellerSync.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
